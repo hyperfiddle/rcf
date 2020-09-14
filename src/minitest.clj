3db97ca9cecfa9f056c7bf90aa333004c79a373b
TristeFigure <funtain@me.com>
2020-09-14 17:07:31 +0200

    Drop eval in favor of thunk fns which can capture the lexical env

--- src/minitest.clj
+++ src/minitest.clj
@@ -6,31 +6,34 @@
 (declare tests test!)
 (def ^:dynamic *tests* (atom {}))
 
+(def ^:private ->|      #(apply comp (reverse %&)))
+(def ^:private as-thunk #(do `(fn [] ~%)))
+(def ^:private as-quote #(do `'~%))
+
 (defmacro tests [& body]
-  ;; One fn per "tests" block
   (let [parsed (->> (partition 3 1 body)
-                    (filter #(-> % second (= '=>)))
-                    (map (juxt first #(nth % 2))))
-        code (for [[form expected] parsed]
-               `(try
-                  (let [v# (eval '~form)]
-                    (set! *3 *2) (set! *2 *1) (set! *1 v#))
-                  (let [e# (eval '~expected)]
-                    (if (= *1 e#)
-                      (println '~'test '~'passed '~form '~'=> '~expected)
-                      (error   '~'test '~'failed '~form '~'=> '~expected e#)))
-                  (catch Exception e#
-                    ;; continue
-                    (error e#))))]
+                    (filter (->| second #{'=>}))
+                    (map (juxt first last))) ;; test & expectation
+        cases  (->> parsed
+                    (map (juxt (->| first  as-quote) (->| first  as-thunk)
+                               (->| second as-quote) (->| second as-thunk))))]
     `(do
-       (swap! *tests* update (ns-name *ns*) (fnil conj []) (do ~@code))
+       (swap! *tests* update (ns-name *ns*) concat ~(vec cases))
        ; if repl mode, just run them
        (test!))))                                             ; danger
+
+(defn- run-test! [[test-form test-thunk expect-form expect-thunk]]
+  (let [test-v   (test-thunk)
+        _        (do (set! *3 *2) (set! *2 *1) (set! *1 test-v))
+        expect-v (expect-thunk)]
+    (if (= test-v expect-v)
+      (println "test passed" test-form "=>" expect-v)
+      (error   "test failed" test-form "=>" expect-v expect-form))))
 
 (defn test!
   ([] (test! (ns-name *ns*)))
   ([ns]
-   (run! eval (get @*tests* (ns-name ns)))))
+   (run! run-test! (get @*tests* (ns-name ns)))))
 
 (tests
   1
@@ -43,5 +46,5 @@
   nil => true
   )
 
-(tests 1 => 1)
-(tests (count (get @*tests* (ns-name *ns*) )) => 2)
+(let [a 1]
+  (tests a => 1))
