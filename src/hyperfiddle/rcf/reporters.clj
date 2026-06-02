11b507a7aa982b9c462fc76db5af5e6efb8be677
adham-omran <git@adham-omran.com>
2026-06-02 15:48:41 +0300

    [rcf]: Setup custom reporters

--- src/hyperfiddle/rcf/reporters.clj
+++ src/hyperfiddle/rcf/reporters.clj
@@ -1,15 +1,78 @@
 (ns hyperfiddle.rcf.reporters
-  (:require [clojure.test :as t]
-            [clojure.string :as str]))
+  (:require [clojure.test :as t]))
 
-(defmethod t/report :hyperfiddle.rcf/pass [_m]
-  (t/with-test-out
-    (t/inc-report-counter :pass)
-    (print "✅")
-    (flush)))
+(def ^:dynamic *block-state* nil)
+
+(defonce reporter-registry (atom {}))
+
+(def ^:dynamic *reporter-key* :compact)
+
+(defn- valid-reporter? [r]
+  (and (map? r)
+       (every? #(fn? (get r %)) [:pass :fail :summary])))
+
+(defn register-reporter! [k r]
+  (when-not (valid-reporter? r)
+    (throw (ex-info "Invalid reporter shape; require :pass :fail :summary as fns"
+             {:key k :reporter r})))
+  (swap! reporter-registry assoc k r)
+  k)
+
+(defn set-reporter! [k]
+  (when-not (contains? @reporter-registry k)
+    (throw (ex-info "Unknown reporter"
+             {:key k :available (vec (keys @reporter-registry))})))
+  (alter-var-root #'*reporter-key* (constantly k))
+  k)
+
+(defn list-reporters [] (keys @reporter-registry))
+
+(defmacro with-reporter [k & body]
+  `(let [k# ~k]
+     (when-not (contains? @reporter-registry k#)
+       (throw (ex-info "Unknown reporter"
+                {:key k# :available (vec (keys @reporter-registry))})))
+     (binding [*reporter-key* k#] ~@body)))
+
+(def compact-reporter
+  {:pass (fn [_m]
+           (when (thread-bound? #'*block-state*)
+             (swap! *block-state* update :pass inc)))
+   :fail (fn [m]
+           (when (thread-bound? #'*block-state*)
+             (swap! *block-state*
+               (fn [s] (-> s
+                         (update :fail inc)
+                         (update :fails conj (select-keys m [:file :line :expected :actual])))))))
+   :summary (fn [m]
+              (let [{:keys [pass fail fails]} m]
+                (when (or (pos? pass) (pos? fail))
+                  (t/with-test-out
+                    (when (pos? pass) (println (str "Pass: " pass)))
+                    (when (pos? fail)
+                      (println (str "Failed: " fail))
+                      (doseq [{:keys [file line expected actual]} fails]
+                        (println (str "- " file ":" line))
+                        (println (str "   - expected: " (pr-str expected)))
+                        (println (str "   - actual: " (pr-str actual)))))))))})
+
+(register-reporter! :compact compact-reporter)
+
+(def dopamine-reporter
+  {:pass (fn [_m]
+           (t/inc-report-counter :pass)
+           (t/with-test-out (println "✅")))
+   :fail (fn [m]
+           (t/report (assoc m :type :fail)))
+   :summary (fn [_m])})
+
+(register-reporter! :dopamine dopamine-reporter)
+
+(defmethod t/report :hyperfiddle.rcf/pass [m]
+  ((:pass (get @reporter-registry *reporter-key*)) m))
 
 (defmethod t/report :hyperfiddle.rcf/fail [m]
-  (print "❌ ")
-  (print (str/triml (with-out-str
-                      (binding [t/*test-out* *out*]
-                        (t/report (assoc m :type :fail)))))))
+  ((:fail (get @reporter-registry *reporter-key*)) m))
+
+(defmethod t/report :hyperfiddle.rcf/summary [m]
+  ((:summary (get @reporter-registry *reporter-key*)) m))
--- src/hyperfiddle/rcf/reporters.cljs
+++ src/hyperfiddle/rcf/reporters.cljs
@@ -1,5 +1,68 @@
 (ns hyperfiddle.rcf.reporters
+  (:require-macros [hyperfiddle.rcf.reporters :refer [with-reporter]])
   (:require [cljs.test :as t]))
+
+(def ^:dynamic *reporter-key* :compact)
+
+(defonce reporter-registry (atom {}))
+
+(defn- valid-reporter? [r]
+  (and (map? r)
+       (every? #(fn? (get r %)) [:pass :fail :summary])))
+
+(defn register-reporter! [k r]
+  (when-not (valid-reporter? r)
+    (throw (ex-info "Invalid reporter shape; require :pass :fail :summary as fns"
+             {:key k :reporter r})))
+  (swap! reporter-registry assoc k r)
+  k)
+
+(defn set-reporter! [k]
+  (when-not (contains? @reporter-registry k)
+    (throw (ex-info "Unknown reporter"
+             {:key k :available (vec (keys @reporter-registry))})))
+  (set! *reporter-key* k)
+  k)
+
+(defn list-reporters [] (keys @reporter-registry))
+
+(def ^:private block-state-path
+  [:hyperfiddle.rcf.reporters/block-state])
+
+(defn- block-state-atom []
+  (get-in (t/get-current-env) block-state-path))
+
+(def compact-reporter
+  {:pass (fn [_m]
+           (when-let [!s (block-state-atom)]
+             (swap! !s update :pass inc)))
+   :fail (fn [m]
+           (when-let [!s (block-state-atom)]
+             (swap! !s
+               (fn [s] (-> s
+                         (update :fail inc)
+                         (update :fails conj (select-keys m [:file :line :expected :actual])))))))
+   :summary (fn [m]
+              (let [{:keys [pass fail fails]} m]
+                (when (or (pos? pass) (pos? fail))
+                  (when (pos? pass) (println (str "Pass: " pass)))
+                  (when (pos? fail)
+                    (println (str "Failed: " fail))
+                    (doseq [{:keys [file line expected actual]} fails]
+                      (println (str "- " file ":" line))
+                      (println (str "   - expected: " (pr-str expected)))
+                      (println (str "   - actual: " (pr-str actual))))))))})
+
+(def dopamine-reporter
+  {:pass (fn [_m]
+           (t/inc-report-counter! :pass)
+           (js/console.log "✅"))
+   :fail (fn [m]
+           (t/report (assoc m :type :fail)))
+   :summary (fn [_m])})
+
+(register-reporter! :compact compact-reporter)
+(register-reporter! :dopamine dopamine-reporter)
 
 (defn testing-vars-str
   "Returns a string representation of the current test.  Renders names
@@ -9,15 +72,20 @@
   (let [{:keys [file line column]} m]
     (str file ":" line (when column (str ":" column)))))
 
-;; For js console.
 (defmethod t/report [::t/default :hyperfiddle.rcf/pass] [m]
-  (t/inc-report-counter! :pass)
-  (js/console.log "✅"))
+  ((:pass (get @reporter-registry *reporter-key*)) m))
 
 (defmethod t/report [::t/default :hyperfiddle.rcf/fail] [m]
-  (t/report (assoc m :type :fail)))
+  ((:fail (get @reporter-registry *reporter-key*)) m))
 
-;; Shadow cljs + karma test runner
+(defmethod t/report [::t/default :hyperfiddle.rcf/summary] [m]
+  ((:summary (get @reporter-registry *reporter-key*)) m))
+
+(defmethod t/report [::t/default :end-test-var] [_m]
+  (when-let [!s (block-state-atom)]
+    (t/report (assoc @!s :type :hyperfiddle.rcf/summary))
+    (t/update-current-env! block-state-path (constantly nil))))
+
 (defmethod t/report [:shadow.test.karma/karma :hyperfiddle.rcf/pass] [m]
   (t/report (assoc m :type :pass)))
 
